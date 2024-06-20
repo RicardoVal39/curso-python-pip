60dfa513225f96c51e565cbe8420ce872205d8a9
RicardoValencia <ricardo.valencia@unillanos.edu.co>
2024-06-20 13:42:04 -0500

    add envs to apps

--- appCSV/utils.py
+++ appCSV/utils.py
@@ -1,14 +1,17 @@
+
 def get_population(country):
     population = {}
-    population['2022'] = country['2022 Population']
-    population['2020'] = country['2020 Population']
-    population['2015'] = country['2015 Population']
-    population['2010'] = country['2010 Population']
-    population['2000'] = country['2000 Population']
-    population['1990'] = country['1990 Population']
-    population['1980'] = country['1980 Population']
-    population['1970'] = country['1970 Population']
-    return population 
+    population['2022'] = int(country['2022 Population'])
+    population['2020'] = int(country['2020 Population'])
+    population['2015'] = int(country['2015 Population'])
+    population['2010'] = int(country['2010 Population'])
+    population['2000'] = int(country['2000 Population'])
+    population['1990'] = int(country['1990 Population'])
+    population['1980'] = int(country['1980 Population'])
+    population['1970'] = int(country['1970 Population'])
+    keys = population.keys()
+    values = population.values()
+    return keys , values  
 
 def population_by_country(data, country):
     data = list(filter(lambda x:x['Country'] == country, data))
