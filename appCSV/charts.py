62a16993d9bd8f4ee9efcae5c7b8fa10fade9f8b
RicardoValencia <ricardo.valencia@unillanos.edu.co>
2024-06-20 13:12:14 -0500

    add app charts

---
+++ appCSV/charts.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def generate_pie_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    plt.savefig('pie.png')
+    plt.close()
+
+def generate_bar_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.savefig('bar.png')
+    plt.close()
---
+++ appCSV/utils.py
@@ -0,0 +1,17 @@
+import 
+
+def get_population(country):
+    population = {}
+    population['2022'] = country['2022 Population']
+    population['2020'] = country['2020 Population']
+    population['2015'] = country['2015 Population']
+    population['2010'] = country['2010 Population']
+    population['2000'] = country['2000 Population']
+    population['1990'] = country['1990 Population']
+    population['1980'] = country['1980 Population']
+    population['1970'] = country['1970 Population']
+    return population 
+
+def population_by_country(data, country):
+    data = list(filter(lambda x:x['Country'] == country, data))
+    return data
